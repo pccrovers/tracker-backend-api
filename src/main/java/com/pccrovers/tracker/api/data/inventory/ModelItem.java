d69d9cc73f083e7f2eacc56d7801ce6c16f9cd51
Steve Lam <stevyo99@yahoo.ca>
2015-10-21 00:57:21 -0700

    forgot to populate the model for inventory items

--- src/main/java/com/pccrovers/tracker/api/data/inventory/ModelItem.java
+++ src/main/java/com/pccrovers/tracker/api/data/inventory/ModelItem.java
@@ -4,5 +4,38 @@ import com.pccrovers.tracker.api.data.BaseModel;
 
 public class ModelItem extends BaseModel
 {
+    /**
+     * The class of item (i.e. lantern, stove, knife)
+     */
+    public String genus;
 
+    /**
+     * The brand or manufacturer of the item (i.e. primus, sunbeam, stovetec)
+     */
+    public String brand;
+
+    /**
+     * The item name (i.e. firefly stove, propane lantern)
+     */
+    public String product;
+
+    /**
+     * The model of the item
+     */
+    public String model;
+
+    /**
+     * The serial number of the item
+     */
+    public String serialNumber;
+
+    /**
+     * The nth item of the set, could be used for identifying otherwise identical items
+     */
+    public Integer number;
+
+    /**
+     * The id of the user whom this item is lent to
+     */
+    public Long borrowerUserId;
 }
